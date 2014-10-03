af1d79947b82deb2c26408ea9e42dfd579813907
vsunder <vsunder@Jeeves.(none)>
2014-10-03 13:09:35 -0400

    Pusher Plugin added to demo quasistatic plugin

---
+++ plugin_test/include/Pusher_Plugin.hh
@@ -0,0 +1,27 @@
+#ifndef _PUSHER_PLUGIN_HH_
+#define _PUSHER_PLUGIN_HH_
+
+#include "gazebo/gazebo.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include "Quasistatic_World_Plugin.hh"
+#include "gazebo/sensors/sensors.hh"
+#include "gazebo/transport/transport.hh" 
+#include "gazebo/transport/transport.hh"
+#include "sdf/sdf.hh"
+
+namespace gazebo
+{
+	class Pusher_Plugin : public ModelPlugin
+	{
+	public:
+	  Pusher_Plugin();
+      void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+      void OnUpdate(const common::UpdateInfo & /*_info*/);
+	private: 
+	  physics::ModelPtr pusher_;
+	  event::ConnectionPtr update_connection_;      
+
+	};
+};
+#endif 
---
+++ plugin_test/src/Pusher_Plugin.cc
@@ -0,0 +1,30 @@
+#include "Pusher_Plugin.hh"
+#include "gazebo/gazebo.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/common/common.hh"
+#include "Quasistatic_World_Plugin.hh"
+#include "gazebo/sensors/sensors.hh"
+#include "gazebo/transport/transport.hh" 
+#include "gazebo/transport/transport.hh"
+#include "sdf/sdf.hh"
+
+using namespace gazebo;
+GZ_REGISTER_MODEL_PLUGIN(Pusher_Plugin)
+
+Pusher_Plugin::Pusher_Plugin()
+{}
+
+void Pusher_Plugin::Load(physics::ModelPtr _parent, sdf::ElementPtr /*sdf*/)
+{
+	this->pusher_ = _parent;
+	  /* Listen to 'step' events */
+  	this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&Pusher_Plugin::OnUpdate, this, _1));
+}
+
+void Pusher_Plugin::OnUpdate(const common::UpdateInfo & /*_info*/)
+{
+	pusher_->SetLinearVel(math::Vector3(0.03,0,0));
+}
+
+
