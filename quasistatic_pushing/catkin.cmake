14106973d1bcce7a9ece36ea95198b1280b62172
Jennifer King <jeking@andrew.cmu.edu>
2015-05-21 14:06:42 -0400

    More updates for indigo upgrades

--- quasistatic_pushing/catkin.cmake
+++ quasistatic_pushing/catkin.cmake
@@ -11,6 +11,15 @@ catkin_package(
 find_package(cmake_modules REQUIRED)
 find_package(Eigen REQUIRED)
 
+include(FindPkgConfig)
+pkg_check_modules(YamlCpp REQUIRED yaml-cpp)
+if (${YamlCpp_VERSION} VERSION_LESS 0.5.0)
+    message(STATUS "Using the old-style yaml-cpp (< 0.5.0) API.")
+else ()
+    add_definitions(-DYAMLCPP_NEWAPI)
+    message(STATUS "Using the new-style yaml-cpp (>= 0.5.0) API.")
+endif ()
+
 include_directories(
     "include/quasistatic_pushing"
     ${catkin_INCLUDE_DIRS}
